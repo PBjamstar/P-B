eff1660acbca8148ed0517361c5f7dff5b5696f2
PBjamstar <jamiefirth1989@gmail.com>
2026-05-08 23:06:49 +0100

    Add files via upload

--- fetch-discord-data.js
+++ fetch-discord-data.js
@@ -2,6 +2,6 @@
   "updatedAt": "2026-01-01T00:00:00.000Z",
   "totalMembers": 0,
   "trialCount": 0,
-  "guildName": "P-B x Phantom Bloodline",
-  "ranks": []
+  "onlineCount": 0,
+  "totalServerCount": 0
 }
